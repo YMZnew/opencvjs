0638df0eb6972bb27623c5a947486002ce4392b6
YMZnew <53408011+YMZnew@users.noreply.github.com>
2020-11-20 16:46:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function setupVideo(displayVid, displayOverlay, setupCallback) {
     window.videoElem.setAttribute("playsinline", "");
 
     navigator.mediaDevices.getUserMedia({
-        video: { facingMode:  exact: "environment" },
+        video: { width: 1280, height: 720 },
         audio: false
     })
     .then(stream => {
